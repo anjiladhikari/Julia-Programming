3c7e7e7a18fac5e21814199e8dc1f83ff52498fc
Anjil Adhikari <anjiladlee10@gmail.com>
2022-01-13 16:44:49 +0545

    Julia

---
+++ Julia/Basic of julia.jl
@@ -0,0 +1,24 @@
+#lets print hello world
+
+println("Hello world")
+
+#Basic math
+
+sum = 13 + 70
+difference =2- 3
+product = 6 * 5
+quotient = 200 / 10
+power = 20 ^ 2
+modulus = 503 % 2
+
+println("Sum = ",sum,
+"\n",
+"Difference = ", difference,
+"\n",
+  "Product = ", product,
+	"\n", 
+	"quotient = ", quotient,
+	"\n",
+	"power = " ,power,
+	"\n",
+	"modulus = ",modulus)
